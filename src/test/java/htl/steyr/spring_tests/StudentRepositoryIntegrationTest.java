c62110ce5498e6459bdf196f20897e91765d5077
mjaeger293 <mjaeger@htl-steyr.ac.at>
2022-11-16 08:58:52 +0100

    Changed test

--- src/test/java/htl/steyr/spring_tests/StudentRepositoryIntegrationTest.java
+++ src/test/java/htl/steyr/spring_tests/StudentRepositoryIntegrationTest.java
@@ -4,15 +4,21 @@ import htl.steyr.spring_tests.models.Student;
 import htl.steyr.spring_tests.models.repositories.StudentRepository;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+//@DataJpaTest
+@SpringBootTest(classes = SpringTestsApplication.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestPropertySource(locations = "classpath:test.properties")
 public class StudentRepositoryIntegrationTest {
     @Autowired
     StudentRepository studentRepository;
